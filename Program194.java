7f5bf57e9a811276dc93b165c7021355204398fb
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-08 08:24:27 -0700

    Program to display addition of two numbers taking input in hardcoded level.

---
+++ Program194.java
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name : main()
+//  Input: 
+//  Output: 
+//  Description : Program to display addition of two numbers taking input in hardcoded level.
+//  Date: 08/08/2021
+//  Author: Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////
+ 
+class Program194
+{
+	public static void main(String arg[])
+	{
+		int ans = 0;
+		int no1 = 10, no2 = 20;
+		ans = no1 + no2;
+		System.out.println("Addition is : "+ans);
+	}
+}
