0750af9e63090b33fa9eed5e669fb2533feaebb9
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-08 08:23:47 -0700

    Program to display Hello world!

---
+++ Program193.java
@@ -0,0 +1,19 @@
+
+/////////////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name : main()
+//  Input: ----
+//  Output: Hello World
+//  Description : Program to display Hello world!
+//  Date: 08/08/2021
+//  Author: Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////
+
+class Program193
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello world");
+	}
+}
