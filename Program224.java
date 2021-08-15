e66112622405d03abd5bec36524d2093507726e8
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-15 03:40:48 -0700

    To Find Length of String and trim the String using trim function.

---
+++ Program224.java
@@ -0,0 +1,31 @@
+
+///////////////////////////////////////////////////////////
+//
+//  Function Name : main()
+//  Description   : To Find Length of String and trim the String using trim function.
+//  Input         : String
+//  Output        : String,Integer
+//  Author        : Hrutik Takke
+//  Date          : 15/08/2021
+//
+//////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Program224
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sobj.nextLine();
+		System.out.println("String length is : "+str.length());
+		String temp = str.trim();
+		System.out.println("New string is : "+temp);
+		System.out.println("Length of new string is : "+temp.length());
+	}
+}
+
+
+
+
