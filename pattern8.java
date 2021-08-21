2f62feee6dbf847aff6103bb928bb17e99f70c82
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-20 19:29:07 -0700

    Display below pattern.

---
+++ pattern8.java
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////
+//
+//  Function name : Design
+//  Description  : Display below pattern.
+//  Input        : iRow = 3 iCol = 4  
+//
+//  Output       : 1 1 1 1 
+//                 2 2 2 2 
+//                 3 3 3 3 
+//                 4 4 4 4 
+//  Author       : Hritik Takke
+//  Date         : 21/08/2021
+//
+/////////////////////////////////////////////////////
+import java.util.*;
+
+class Pattern
+{
+	public void Design(int iRow,int iCol)
+	{
+       for(int i=1;i<=iRow;i++)
+       {
+       	 for(int j=1;j<=iCol;j++)
+       	 {
+       	 	System.out.print(i+"\t");
+       	 }
+       	 System.out.println();
+       }
+	}
+}
+
+class P5
+{
+	public static void main(String args[]) 
+	{
+		Scanner Sobj=new Scanner(System.in);
+
+		System.out.println("Enter the Row");
+		int row=Sobj.nextInt();
+		System.out.println("Enter the Colum");
+		int col=Sobj.nextInt();
+
+		Pattern obj=new Pattern();
+		obj.Design(row,col);
+		
+	}
+}
