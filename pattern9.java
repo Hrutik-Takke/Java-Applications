316661f32ea430e4c10683dbcf528e75f4bf40b8
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-21 07:41:28 -0700

    Display below pattern.

---
+++ pattern9.java
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////
+//
+//  Function name : Design
+//  Description  : Display below pattern.
+//  Input        : Hello   
+//
+//  Output       : H e l l o 
+//                 H e l l o 
+//                 H e l l o 
+//                 H e l l o
+//  Author       : Hritik Takke
+//  Date         : 21/08/2021
+//
+/////////////////////////////////////////////////////
+import java.util.*;
+
+class Pattern
+{
+	public void Design(String str)
+	{
+      char st[]=str.toCharArray();
+
+      for(int i=0;i<st.length;i++)
+      {
+      	for(int j=0;j<st.length;j++)
+      	{
+      		System.out.print(st[j]+"\t");
+      	}
+      	System.out.println();
+      }
+	}
+}
+
+class P1
+{
+	public static void main(String[] args) 
+	{
+	   Scanner Sobj=new Scanner(System.in);
+
+	   System.out.println("Enter the String ");
+	   String str=Sobj.nextLine();
+
+	   Pattern obj=new Pattern();
+	   obj.Design(str);
+
+	}
+}
