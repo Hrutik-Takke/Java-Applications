cf61df5226c69e56514226ab686e60edcc4cd2a8
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-26 09:05:24 -0700

    Take two arguments are command line arguments and swap them by using 3rd Variable.

---
+++ Swapping.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Swapping
+//  Input : 10  20 
+//  Output : 20 10 
+//  Description : Take two arguments are command line arguments and swap them by using 3rd Variable.
+//  Date : 26/08/2021
+//  Author : Hritik Takkes
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+class Swapping
+{
+	public static void main(String args[])
+	{
+		float i,j,temps;
+		int count;
+		count=args.length;
+		System.out.println(count);
+		i=float.parseFloat(args[0]);
+		j=float.parseFloat(args[1]);
+		
+		System.out.println("The values are "+i+""+j);
+		temps=i;
+		i=j;
+		j=temps;
+		System.out.println("The Swapped Values are"+i+""+j);
+	}
+}
