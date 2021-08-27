9a9c5d69fa7adf0f0f7808380b394961d7fa5c76
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-27 07:17:41 -0700

    Implementation of sum of number.

---
+++ factorial.java
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////
+//
+//  Function : factorial
+//  Input : Enter the no 5
+//  Output : factorial is 120
+//  Description : Implementation of sum of number.
+//  Date : 27/08/2021
+//  Author Name : Hritik Takke
+//
+////////////////////////////////////////////////////
+	
+package functionmain;
+
+import java.util.Scanner;
+
+public class factorial 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int n=sc.nextInt();
+		factorial(n);
+    }
+	public static void factorial(int no)
+	{
+		int fact=1;
+		for(int i=1;i<=no;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("factorial is "+fact);
+	}
+
+}
