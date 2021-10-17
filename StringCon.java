13fdbc05876d1bac3f4decc74c103443220ee316
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-10-17 10:37:47 -0700

    program to Concatenate two Strings.

---
+++ StringCon.java
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////
+//
+//  Function Name: StringCon
+//  Input: --
+//  Output: Good Morning..! from Hrutik Takke
+//  Description: program to Concanate two String. 
+//  Date: 17/10/2021
+//  Author: Hrutik Takke
+//
+///////////////////////////////////////////////////////
+
+class StringCon
+{
+	public static void main(String[] args) 
+	{
+		String str="Good Morning..!";
+		String st="  from Hrutik Takke";
+		String Con= str+st;
+	    System.out.println(Con);
+
+	}
+}
